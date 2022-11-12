6f9a32154c9847cc0558e817f5d7bd6797696a64
Alexandru Ion <ion.alexandruc99@gmail.com>
2022-11-13 00:40:39 +0200

    Main implementation for the Filtering sistem added

---
+++ src/EasyFilters/FilterService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyFilters
+{
+    /// <inheritdoc />
+    public sealed class FilterService<TEnumerable, TData> : IFilterService<TEnumerable, TData> where TEnumerable : class, IEnumerable<TData>, new() where TData : class
+	{
+        private readonly List<IFilter<TEnumerable, TData>> _filters;
+
+        public FilterService()
+        {
+            _filters = new List<IFilter<TEnumerable, TData>>();
+        }
+
+        /// <inheritdoc />
+        public IFilterService<TEnumerable, TData> AddFilter<TFilterType>(BaseFilterProperties filterProperties = null) where TFilterType : IFilter<TEnumerable, TData>, new()
+        {
+            var filter = new TFilterType();
+
+            filter.Initialize(filterProperties);
+
+            _filters.Add(filter);
+
+            return this;
+        }
+
+        /// <inheritdoc />
+        public async Task<TEnumerable> ExecuteAsync(TEnumerable elements)
+        {
+            var copyOfElements = MakeCopyEnumerable(elements);
+
+            foreach(var filter in _filters)
+            {
+                copyOfElements = await filter.ExecuteFilter(copyOfElements);
+            }
+
+            return copyOfElements;
+        }
+
+        private static TEnumerable MakeCopyEnumerable(TEnumerable elements)
+        {
+            var copyOfElements = new TEnumerable();
+
+            foreach (var element in elements)
+            {
+                copyOfElements = copyOfElements.Append(element) as TEnumerable;
+            }
+
+            return copyOfElements;
+        }
+    }
+}
+
---
+++ src/EasyFilters/IFilterService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EasyFilters
+{
+
+    /// <summary>
+    /// The core service used for filtering the data. In this service the implementations of the IFilter interface must be added.
+    /// </summary>
+    /// <typeparam name="TEnumarable">The type of IEnumarable for which the filtering is done. Ex: List, Dictionary.</typeparam>
+    /// <typeparam name="TData">The data type contained in the IEnumarable type</typeparam>
+    public interface IFilterService<TEnumarable, TData> where TEnumarable : IEnumerable<TData> where TData : class
+    {
+        /// <summary>
+        /// Adds a new filter in the List of filters that the service has and initializes it with the received Filter Properties
+        /// </summary>
+        /// <typeparam name="TFilterType">An implementation of the IFilter interface that has the same TData type as the IFilterService</typeparam>
+        /// <param name="filterProperties">The properties that the mentioned filter should take into consideration when running</param>
+        /// <returns>Returns the same instant of an object. Used for chaning methods.</returns>
+        IFilterService<TEnumarable, TData> AddFilter<TFilterType>(BaseFilterProperties filterProperties = null)
+            where TFilterType : IFilter<TEnumarable, TData>, new();
+
+        /// <summary>
+        /// Executes all the filters that are in the service List of filters. The filter are executed in the order they were added.
+        /// </summary>
+        /// <param name="elements">An IEnumarable </param>
+        /// <returns></returns>
+        Task<TEnumarable> ExecuteAsync(TEnumarable elements);
+    }
+}
